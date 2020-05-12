f049d04ec2b97bd662eef2e5a736881bee368d58
VIT1050 <ducviet321@gmail.com>
2020-05-13 00:49:23 +0700

    Add video sample

--- gi/lib/index.js
+++ gi/lib/index.js
@@ -1,10 +1,16 @@
 (() => {
     let header = document.querySelector('header')
+    let introVideo = document.querySelector('#introVideo')
 
     let init = () => {
+        introVideo.oncanplay = () => {
+            console.log('video ready sir')
+        }
+
+        // Wait for video intro
         setTimeout(() => {
             header.className = "show"
-        }, 1000)
+        }, 5000)
     }
 
     init()
